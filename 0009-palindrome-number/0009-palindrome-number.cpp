646825d0cd1a1232408e793cc0238789c02ed2a9
Devansh <itsdevansh16@gmail.com>
2026-06-22 21:01:38 +0530

    Time: 0 ms (100%), Space: 8.5 MB (65.64%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0)
+        {
+            return false;
+        }
+
+        int temp = x;
+        long rev = 0;
+        while(temp != 0)
+        {
+            int digit = temp%10;
+            rev = rev*10 + digit;
+            temp = temp/10;
+        }
+
+        if(rev == x)
+        {
+            return true;
+        }
+
+        return false;
+    }
+};
