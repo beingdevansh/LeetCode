12f2a88950574f07a00a93dfbd1afbb3e8c5c4ff
Devansh <itsdevansh16@gmail.com>
2026-07-02 20:42:15 +0530

    Time: 0 ms (100%), Space: 7.7 MB (58.02%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
     bool isStrictlyPalindromic(int n) {
-        for (int i = 2; i <= n - 2; i++) 
-        {
-            int temp = n;
-            string result = "";
-            while (temp > 0) {
-                int digit = temp % i;
-                result += to_string(digit);
-                temp /= i;
-            }
-
-            string rev = result;
-            reverse(rev.begin(), rev.end());
-            if (result != rev) return false;
-        }
-        return true;
+        return false;
     }
 };
