25981a7f66f16709b6863db0a60d85092d52f6ac
Devansh <itsdevansh16@gmail.com>
2026-07-02 18:05:44 +0530

    Time: 4 ms (3.49%), Space: 17.5 MB (74.91%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -5,10 +5,14 @@ public:
         int x = 0;
         for(int i = 0; i < n; i++)
         {
-            if(operations[i] == "++X") {++x;}
-            if(operations[i] == "X++") {x++;}
-            if(operations[i] == "--X") {--x;}
-            if(operations[i] == "X--") {x--;}
+            if(operations[i] == "++X" || operations[i] == "X++")
+            {
+                x += 1;
+            }
+            else
+            {
+                x -= 1;
+            }
         }
 
         return x;
