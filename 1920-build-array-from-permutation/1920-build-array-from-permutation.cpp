dbd19e2830fb1887512c15e35529f81cc89817c0
Devansh <itsdevansh16@gmail.com>
2026-07-02 11:29:13 +0530

    Time: 0 ms (100%), Space: 20.5 MB (62.41%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
-        vector <int> ans(nums.size());
+        vector <int> ans(nums.size(),0);
         for(int i = 0; i < nums.size() ; i++)
         {
             ans[i] = nums[nums[i]];
