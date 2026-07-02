b4969f4fc4f8b910ba36512442f276598b77d2ed
Devansh <itsdevansh16@gmail.com>
2026-07-02 21:17:58 +0530

    Time: 1 ms (98.88%), Space: 46.1 MB (93.21%) - LeetHub

--- 3701-compute-alternating-sum/3701-compute-alternating-sum.java
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.java
@@ -4,14 +4,10 @@ class Solution {
         for(int  i = 0; i < nums.length; i++)
         {
             if(i%2 == 0)
-            {
                 sum += nums[i];
-            }
 
             else
-            {
                 sum -= nums[i];
-            }
         }
 
         return sum;
