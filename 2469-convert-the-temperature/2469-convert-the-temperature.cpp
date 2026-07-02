27af8f1a2323e5dc4e5e133243c4e770464ef79e
Devansh <itsdevansh16@gmail.com>
2026-07-02 20:57:43 +0530

    Time: 0 ms (100%), Space: 8.5 MB (10.81%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.cpp
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     vector<double> convertTemperature(double celsius) {
-        vector <double> ans;
-        double k, f;
-        k = celsius + 273.15;
-        ans.push_back(k);
-        f = (celsius*1.80) + 32.00;
-        ans.push_back(f);
-
-        return ans;
+        return {celsius + 273.15, celsius * 1.80 + 32.00};
     }
 };
