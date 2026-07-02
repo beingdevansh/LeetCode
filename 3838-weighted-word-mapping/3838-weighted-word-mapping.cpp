43ce1e5fe709d84edfd7576dc75d6eb55ce4fef0
Devansh <itsdevansh16@gmail.com>
2026-07-02 18:31:48 +0530

    Time: 0 ms (100%), Space: 43.6 MB (81.19%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
         string res = "";
-        for(int  i = 0; i < words.size(); i++)
+        for(auto str : words)
         {
             int sum = 0;
-            string word = words[i];
-            for(char ch : word)
+            for(char ch : str)
             {
                 sum  += weights[ch - 'a'];
             }
-            
-            int mod = sum%26;
-            char c = 'z' - mod;
-            res += c;
+
+            sum %= 26;
+            res += ('z' - sum);
         }
 
         return res;
