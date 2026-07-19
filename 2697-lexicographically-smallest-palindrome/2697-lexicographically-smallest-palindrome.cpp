4eb6e19a5468ce1a5a885d9f428d1e617be68966
Devansh <itsdevansh16@gmail.com>
2026-07-19 18:24:41 +0530

    Time: 7 ms (40.1%), Space: 18.6 MB (25.72%) - LeetHub

---
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int i = 0;
+        int j = s.size() - 1;
+        while(i <= j)
+        {
+            if(s[i] != s[j])
+            {
+                char ch = min(s[i], s[j]);
+                s[i] = ch;
+                s[j] = ch;
+            }
+
+            i++;
+            j--;
+        }
+
+        return s;
+    }
+};
