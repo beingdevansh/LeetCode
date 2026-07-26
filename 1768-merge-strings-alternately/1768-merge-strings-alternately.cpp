365475f8a167dcec013693f1611cd6a594467849
Devansh <itsdevansh16@gmail.com>
2026-07-26 10:21:55 +0530

    Time: 0 ms (100%), Space: 8.5 MB (64.65%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -2,32 +2,27 @@ class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
         int n;
-        if(word1.size() > word2.size())
-        {
+        if (word1.size() > word2.size()) {
             n = word2.size();
         }
 
-        else
-        {
+        else {
             n = word1.size();
         }
 
         string res = "";
         int i = 0;
-        while(i < n)
-        {
+        while (i < n) {
             res += word1[i];
             res += word2[i];
             i++;
         }
 
-        if(n == word2.size())
-        {
+        if (n == word2.size()) {
             res += word1.substr(i);
         }
 
-        else
-        {
+        else {
             res += word2.substr(i);
         }
 
