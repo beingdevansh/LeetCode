b06dd230136183333e605e320b3dd1e92b9828fb
Devansh <itsdevansh16@gmail.com>
2026-07-31 23:31:32 +0530

    Time: 11 ms (48.07%), Space: 25.4 MB (84.45%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -8,20 +8,8 @@ public:
 
         int push = 0;
         sort(freq.begin(), freq.end(), greater<int>());
-        for (int i = 0; i < 8; i++) {
-            push += freq[i];
-        }
-
-        for (int i = 8; i < 16; i++) {
-            push += freq[i] * 2;
-        }
-
-        for (int i = 16; i < 24; i++) {
-            push += freq[i] * 3;
-        }
-
-        for (int i = 24; i < 26; i++) {
-            push += freq[i] * 4;
+        for (int i = 0; i < 26; i++) {
+            push += freq[i] * ((i/8) + 1);
         }
 
         return push;
