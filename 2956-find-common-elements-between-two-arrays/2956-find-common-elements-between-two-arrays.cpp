ccadbed8c45686236f330f851388d1dd5d5bfab9
Devansh <itsdevansh16@gmail.com>
2026-08-23 22:28:25 +0530

    Time: 0 ms (100%), Space: 65.9 MB (51.86%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.cpp
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
     vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
-        bool arr1[101] = {false};
-        bool arr2[101] = {false};
+        int arr1[101] = {0};
+        int arr2[101] = {0};
 
-        for (int val : nums1) {
-            arr1[val] = true;
-        }
+        for (int val : nums1) 
+            arr1[val]++;
 
-        for (int val : nums2) {
-            arr2[val] = true;
-        }
+        for (int val : nums2) 
+            arr2[val]++;
 
         int ans1 = 0;
         int ans2 = 0;
 
-        for (int val : nums1) {
-            if (arr2[val]) {
-                ans1++;
-            }
-        }
-
-        for (int val : nums2) {
-            if (arr1[val]) {
-                ans2++;
-            }
-        }
+        for (int i = 0; i <= 100; i++) {
+			if (arr1[i] && arr2[i]) {
+				ans1 += arr1[i];
+				ans2 += arr2[i];
+			}
+		}
 
         return {ans1, ans2};
     }
