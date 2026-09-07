0bc1247e2fdd1c81d864e28bd40837c67b54ab00
Devansh <itsdevansh16@gmail.com>
2026-09-07 20:58:11 +0530

    Time: 0 ms (100%), Space: 7.7 MB (81.86%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -3,9 +3,6 @@ public:
     bool isPowerOfTwo(int n) {
         if(n == 0) return false;
 
-        while(n%2 == 0)
-            n /= 2;
-
-        return n == 1;
+        return (n == 1 || (n%2 == 0 && isPowerOfTwo(n/2)));
     }
 };
